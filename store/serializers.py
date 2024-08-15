2d5195f54a96d6a06a7a5db9b11c814e924e888e
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-08-15 16:41:59 +0330

    add SerializerMethodField [custome fields] & convert decimal ti str from DRF setting

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
+from decimal import Decimal
 
 
 class CategorySerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 
 
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255, source='name')
+    name = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     category = CategorySerializer()
+    price_after_tax = serializers.SerializerMethodField(method_name='get_price_rial')
+
+
+    def get_price_rial(self, product):
+        return round(product.unit_price * Decimal(1.09), 2)
