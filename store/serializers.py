9e47c9270d4bcb937ecb2cb1da83677d6179dfe6
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-08-26 15:47:26 +0330

    user ModelSerializer in serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,25 +1,37 @@
 from rest_framework import serializers
 from decimal import Decimal
+from django.urls import reverse
 
-from .models import Category
+from .models import Category, Product
 
 
-class CategorySerializer(serializers.Serializer):
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = ['title', 'description']
     title = serializers.CharField(max_length=255)
     description = serializers.CharField(max_length=500)
 
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+class ProductSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField()
+    # name = serializers.CharField(max_length=255)
+    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     price_after_tax = serializers.SerializerMethodField(method_name='get_price_rial')
-    category = serializers.HyperlinkedRelatedField(
-        queryset=Category.objects.all(),
-        view_name='category-detail',
-    )
+    category = serializers.HyperlinkedRelatedField(queryset=Category.objects.all(), view_name='category-detail')
+    detail = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'unit_price', 'inventory', 'category', 'price_after_tax', 'detail']
+
 
 
     def get_price_rial(self, product):
         return round(product.unit_price * Decimal(1.09), 2)
+
+    def get_detail(self, product):
+        request = self.context['request']
+        return request.build_absolute_uri(reverse('product-detail', args=[product.pk]))
