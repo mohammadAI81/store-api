dae4f456c7bcc966a5faace3f35876c5bfde979d
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-09-03 15:02:50 +0330

    change method POST convert from product_detail to product_list

--- store/views.py
+++ store/views.py
@@ -6,23 +6,25 @@ from rest_framework.response import Response
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
-@api_view()
-def product_list(request):
-    product = Product.objects.select_related('category')
-    serializer = ProductSerializer(product, many=True, context={'request': request})
-    return Response({'title': 'list of product', 'products': serializer.data})
-
 @api_view(['GET', 'POST'])
-def product_detail(request, pk):
+def product_list(request):
     if request.method == 'GET':
-        serializer = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(serializer, context={'request': request})
-        return Response(serializer.data)
+        product = Product.objects.select_related('category')
+        serializer = ProductSerializer(product, many=True, context={'request': request})
+        return Response({'title': 'list of product', 'products': serializer.data})
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response('Okay')
+
+
+@api_view()
+def product_detail(request, pk):    
+        serializer = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(serializer, context={'request': request})
+        return Response(serializer.data)
+
 
 @api_view()
 def category_detail(request, pk):
