f774679cbff5674d9f9982a157ca2da58c2dc7df
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-10-09 23:26:32 +0330

    change APIView to RetrieveUpdateDestroyAPIView for product & category detail

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
@@ -19,21 +19,10 @@ class ProductList(ListCreateAPIView):
     def get_serializer_context(self):
         return {'request': self.request}
 
-
-class ProductDetail(APIView):
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.select_related('category')
+    serializer_class = ProductSerializer
     
-    def get(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'title': 'Product Is Update', 'description': serializer.data})
-
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.order_items.count()> 0:
@@ -47,22 +36,14 @@ class CategoryList(ListCreateAPIView):
     serializer_class = CategorySerializer
         
 
-class CategoryDetail(APIView):
-    def get(self, request, pk):
-        category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
-        serializer = CategorySerializer(instance=category, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
+class CategoryDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.prefetch_related('products')
+    serializer_class = CategorySerializer
+    
     def delete(self, request, pk):
         category = get_object_or_404(Category.objects.prefetch_related('products'), pk=pk)
         if category.products.count() > 0:
-            return Response({"errors": 'error'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response({"errors": 'error you can not delete'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         category.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
