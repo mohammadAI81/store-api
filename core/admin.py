59d7e94cda01f023b1cabf998f4ef048c066322a
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-11-15 10:26:15 +0330

    set Admin panel for CustomUser

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ['email']}),
+    ) 
+
