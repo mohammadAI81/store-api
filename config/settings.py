95669e7b320d17aca3b41342e3782612b4063b4c
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2025-02-04 14:31:37 +0330

    add djoser JWT & Authentication & set settings for jwt

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'django_filters',
     'rest_framework',
+    'djoser',
     
     'debug_toolbar',
     'store.apps.StoreConfig',
@@ -142,7 +143,13 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     # 'PAGE_SIZE': 10,
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT', ),
+}
 
 AUTH_USER_MODEL = 'core.CustomUser'
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
